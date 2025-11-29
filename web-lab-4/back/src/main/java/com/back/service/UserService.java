ade4c351cae40685c0958e884ab40fb62c50948b
Baili <1172177657@qq.com>
2025-11-29 21:16:29 +0300

    update lab4

--- web-lab-4/back/src/main/java/com/back/service/UserService.java
+++ web-lab-4/back/src/main/java/com/back/service/UserService.java
@@ -1,14 +1,12 @@
 package com.back.service;
 
-import com.back.repository.UserRepository;
+//import com.back.repository.UserRepository;
 import com.back.util.JwtUtil;
 import com.back.util.PasswordEncoder;
 
 public class UserService {
-    private UserRepository userRepository;
+//    private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;  // 密码加密器
     private JwtUtil jwtUtil;
-
-
 
 }
